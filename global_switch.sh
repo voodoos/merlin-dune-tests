2d4f255e31da6d61ab3c0faa5a260daaaa535087
Ulysse Gérard <thevoodoos@gmail.com>
2020-08-25 12:46:36 +0200

    Add missing dep to install script

--- global_switch.sh
+++ global_switch.sh
@@ -1,3 +1,5 @@
 opam pin add dot-merlin-reader.1.0 git+https://github.com/voodoos/merlin.git#ocaml-merlin-errors --yes
 opam pin add merlin.3.3.8 git+https://github.com/voodoos/merlin.git#ocaml-merlin-errors --yes
 opam pin add dune.2.8.0 git+https://github.com/voodoos/dune.git#merlin-abs-paths --yes
+
+opam install base64 conf-jq
