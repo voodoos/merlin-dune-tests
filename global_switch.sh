1a89304bc157317eed655fc72154d7d84a6022f8
Ulysse Gérard <thevoodoos@gmail.com>
2020-09-21 11:27:27 +0200

    Use stable Merlin

--- global_switch.sh
+++ global_switch.sh
@@ -1,5 +1,2 @@
 opam pin add dune.2.8.0 git+https://github.com/voodoos/dune.git#merlin-abs-paths --yes
-opam pin add dot-merlin-reader.1.0 git+https://github.com/voodoos/merlin.git#ocaml-merlin-errors --yes
-opam pin add merlin.3.3.8 git+https://github.com/voodoos/merlin.git#ocaml-merlin-errors --yes
-
-opam install base64 conf-jq ppx_compare ppx_sexp_conv
+opam install merlin base64 conf-jq ppx_compare ppx_sexp_conv
