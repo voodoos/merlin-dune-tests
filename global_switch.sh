ea19a28a961a3ece89b391632553d0792230eb19
Ulysse <5031221+voodoos@users.noreply.github.com>
2021-02-26 13:53:06 +0100

    Update global_switch.sh

--- global_switch.sh
+++ global_switch.sh
@@ -1,4 +1,4 @@
-opam pin add dune.2.8.0 git+https://github.com/ocaml/dune.git --yes
+opam pin add dune.2.8.3 git+https://github.com/voodoos/dune.git#my2.8.3 --yes
 opam install merlin base64 re conf-jq ppx_compare ppx_sexp_conv --yes
 opam update --yes
 opam upgrade --yes
